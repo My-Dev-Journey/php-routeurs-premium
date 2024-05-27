06645837c9dffdf1eef17f74bb62eeed033a3981
Antoine Creuzet <antoine.creuzet@efficientip.com>
2024-05-27 15:40:27 +0200

    Fix: correction oubli d'une condition createMessage

--- controllers/categoryAndTopics.php
+++ controllers/categoryAndTopics.php
@@ -24,6 +24,11 @@ function createMessage()
         // Erreur
     }
 
+    if (empty($_POST['topic_id']) or !is_numeric($_POST['topic_id'])) {
+        // Erreur
+    }
+
+
     addNewMessage($_POST['topic_id'], $_POST['message']);
 
     return header('location: ?action=topic&id=' . $_POST['topic_id']);
