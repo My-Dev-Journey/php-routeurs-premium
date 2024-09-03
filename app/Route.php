97e45ee9797bf156bf9342ad8739e48c80bad068
Antoine Creuzet <antoine.creuzet@efficientip.com>
2024-09-03 08:40:30 +0200

    Base Route

--- app/Route.php
+++ app/Route.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use InvalidArgumentException;
+
 class Route
 {
     private string $action = '';
@@ -10,4 +12,36 @@ class Route
     private string $method = '';
     private array $parameters = [];
     private array $middlewares = [];
+
+    public function __construct(array $data)
+    {
+        foreach ($data as $property => $value) {
+            $setter = 'set' . ucfirst($property);
+            if (method_exists($this, $setter)) {
+                $this->$setter($value);
+            } elseif (property_exists($this, $property)) {
+                $this->$property = $value;
+            } else {
+                throw new InvalidArgumentException('La propriété n\'existe pas');
+            }
+        }
+    }
+
+    public function setController(string $controller)
+    {
+        if (!class_exists($controller)) {
+            throw new InvalidArgumentException('Le controller n\'existe pas');
+        }
+        $this->controller = $controller;
+    }
+
+    public function setMiddlewares(array $middlewares)
+    {
+        foreach ($middlewares as $middleware) {
+            if (!class_exists($middleware)) {
+                throw new InvalidArgumentException('Le middleware n\'existe pas');
+            }
+        }
+        $this->middlewares = $middlewares;
+    }
 }
